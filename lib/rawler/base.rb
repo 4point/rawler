09effd5eb3c4e342dd46514bce473ca5ffd30d0a
Alexander Groß <agross@therightstuff.de>
2016-06-03 18:29:04 +0200

    Report unknown errors (message and type)

--- lib/rawler/base.rb
+++ lib/rawler/base.rb
@@ -86,8 +86,8 @@ module Rawler
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, Errno::ETIMEDOUT,
       EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, SocketError
       error("Connection problems - #{link} - Called from: #{from_url}")
-    rescue Exception
-      error("Unknown error - #{link} - Called from: #{from_url}")
+    rescue Exception => e
+      error("Unknown error #{e} (#{e.class}) - #{link} - Called from: #{from_url}")
     end
 
     def same_domain?(link)
--- spec/lib/rawler_spec.rb
+++ spec/lib/rawler_spec.rb
@@ -6,7 +6,7 @@ describe Rawler::Base do
 
   let(:output)  { double('output').as_null_object }
   let(:rawler)  { Rawler::Base.new('http://example.com', output) }
-  
+
   before(:each) do
     Rawler.stub(:output).and_return(output)
     register('http://example.com', site)
@@ -54,21 +54,21 @@ describe Rawler::Base do
       Rawler::Base.new(original, output)
       Rawler.url.should == expected
     end
-    
+
     it "should auto prepend http" do
       original = 'example.com'
       expected = 'http://example.com'
       Rawler::Base.new(original, output)
       Rawler.url.should == expected
     end
-    
+
     it "should not auto prepend http when already http" do
       original = 'http://example.com'
       expected = 'http://example.com'
       Rawler::Base.new(original, output)
       Rawler.url.should == expected
     end
-    
+
     it "should not auto prepend http when https" do
       original = 'https://example.com'
       expected = 'https://example.com'
@@ -76,9 +76,9 @@ describe Rawler::Base do
       Rawler.url.should == expected
     end
   end
-  
+
   describe "validate_links" do
-    
+
     it "should validate links recursively" do
       register('http://example.com/foo1', '<a href="http://external.com/foo">x</a>')
       register('http://example.com/foo2', '')
@@ -86,36 +86,36 @@ describe Rawler::Base do
       register('http://external.com/foo', '')
 
       rawler.validate
-      
+
       rawler.responses['http://example.com/foo1'].should_not be_nil
       rawler.responses['http://example.com/foo2'].should_not be_nil
       rawler.responses['http://external.com'].should_not be_nil
       rawler.responses['http://external.com/foo'].should_not be_nil
     end
-    
+
     it "should not validate links on external pages" do
       register('http://example.com/', '<a href="http://external.com/foo">x</a>')
       register('http://example.com/foo', '<a href="http://external.com/foo">x</a>')
       register('http://external.com/foo', '<a href="http://external.com/bar">x</a>')
       register('http://external.com/bar', '')
-      
+
       rawler.validate
-      
+
       rawler.responses['http://external.com/foo'].should_not be_nil
       rawler.responses['http://external.com/bar'].should be_nil
     end
-    
+
     it "should output results" do
       register('http://example.com/foo1', '<a href="http://external.com/foo">x</a>')
       register('http://example.com/foo2', '')
       register('http://external.com', '')
       register('http://external.com/foo', '', 301)
-      
+
       output.should_receive(:info).with('200 - http://example.com/foo1')
       output.should_receive(:info).with('200 - http://example.com/foo2')
       output.should_receive(:info).with('200 - http://external.com')
       output.should_receive(:warn).with('301 - http://external.com/foo - Called from: http://example.com/foo1')
-      
+
       rawler.validate
     end
 
@@ -140,53 +140,53 @@ describe Rawler::Base do
     end
 
   end
-  
+
   describe "get_status_code" do
 
     it "should add to 200 links" do
       url = 'http://example.com/foo'
       from = 'http://other.com'
       register(url, '', 200)
-      
+
       rawler.send(:add_status_code, url, from)
-      
+
       rawler.responses[url][:status].should == 200
     end
-    
+
     it "should add to 301 links" do
       url = 'http://example.com/foo'
       from = 'http://other.com'
       register(url, '', 301)
-      
+
       rawler.send(:add_status_code, url, from)
-      
+
       rawler.responses[url][:status].should == 301
     end
-    
+
     it "should save username and password" do
       rawler = Rawler::Base.new('http://example.com', output, {:username => 'my_user', :password => 'secret'})
-      
+
       Rawler.username.should == 'my_user'
       Rawler.password.should == 'secret'
     end
-    
+
     it "should save wait" do
       rawler = Rawler::Base.new('http://example.com', output, {:wait => 5})
-      
+
       Rawler.wait.should == 5
     end
-    
+
     it "should rescue from Errno::ECONNREFUSED" do
       url = 'http://example.com'
       from = 'http://other.com'
-      
+
       Rawler::Request.should_receive(:get).and_raise Errno::ECONNREFUSED
-      
+
       output.should_receive(:error).with("Connection refused - #{url} - Called from: #{from}")
-      
+
       rawler.send(:add_status_code, url, from)
     end
-    
+
     [Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, Errno::ETIMEDOUT, EOFError,
     Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, SocketError].each do |error|
        it "should rescue from #{error}" do
@@ -198,28 +198,28 @@ describe Rawler::Base do
          output.should_receive(:error).with("Connection problems - #{url} - Called from: #{from}")
 
          rawler.send(:add_status_code, url, from)
-       end   
+       end
     end
 
     it "should rescue from general errors" do
       url = 'http://example.com'
       from = 'http://other.com'
-      
-      Rawler::Request.should_receive(:get).and_raise
-      
-      output.should_receive(:error).with("Unknown error - #{url} - Called from: #{from}")
-      
+
+      Rawler::Request.should_receive(:get).and_raise('foo')
+
+      output.should_receive(:error).with("Unknown error foo (RuntimeError) - #{url} - Called from: #{from}")
+
       rawler.send(:add_status_code, url, from)
     end
 
-    
+
   end
-  
+
   describe "record_response" do
-    
+
     let(:link) { 'http://foo.com' }
     let(:from) { 'http://bar.com' }
-    
+
     context "response code 100" do
       %w!100, 150, 199!.each do |code|
 
@@ -227,10 +227,10 @@ describe Rawler::Base do
           output.should_receive(:info).with("#{code} - #{link}")
           rawler.send(:record_response, code, link, from)
         end
-        
+
       end
     end
-    
+
     context "response code 200" do
       %w!200, 250, 299!.each do |code|
 
@@ -238,10 +238,10 @@ describe Rawler::Base do
           output.should_receive(:info).with("#{code} - #{link}")
           rawler.send(:record_response, code, link, from)
         end
-        
+
       end
     end
-    
+
     context "response code 300" do
       %w!300, 350, 399!.each do |code|
 
@@ -249,10 +249,10 @@ describe Rawler::Base do
           output.should_receive(:warn).with("#{code} - #{link} - Called from: #{from}")
           rawler.send(:record_response, code, link, from)
         end
-        
+
       end
     end
-    
+
     context "response code 400" do
       %w!400, 450, 499!.each do |code|
 
@@ -260,10 +260,10 @@ describe Rawler::Base do
           output.should_receive(:error).with("#{code} - #{link} - Called from: #{from}")
           rawler.send(:record_response, code, link, from)
         end
-        
+
       end
     end
-    
+
     context "response code 500" do
       %w!400, 550, 599!.each do |code|
 
@@ -271,18 +271,18 @@ describe Rawler::Base do
           output.should_receive(:error).with("#{code} - #{link} - Called from: #{from}")
           rawler.send(:record_response, code, link, from)
         end
-        
+
       end
     end
-    
+
     context "response code invalid" do
       let(:code) { 600 }
-      
+
       it "logger should receive eror" do
         output.should_receive(:error).with("Unknown code #{code} - #{link} - Called from: #{from}")
         rawler.send(:record_response, code, link, from)
       end
-    end    
+    end
   end
 
   describe "error introspection" do
@@ -311,10 +311,10 @@ describe Rawler::Base do
     end
   end
 
-  
-  
+
+
   private
-  
+
   def site
     <<-site
       <html>
@@ -327,5 +327,5 @@ describe Rawler::Base do
       </html>
     site
   end
-  
+
 end
